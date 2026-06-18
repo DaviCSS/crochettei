7883f846d21715ea3cef6052beaef4ef2ff659db
DaviCSS <125618959+DaviCSS@users.noreply.github.com>
2026-06-17 22:03:34 -0300

    limpeza 2

--- check_products.php
+++
@@ -1,6 +0,0 @@
-<?php
-require 'public/index.php';
-$db = \Config\Database::connect();
-$res = $db->query('SELECT COUNT(*) as c FROM produtos');
-$row = $res->getRow();
-echo 'Products: ' . $row->c;
